66a5389e24c81b6196347e0b45b687410def2a01
Tucker Steil <tuckersteil@gmail.com>
2023-01-05 15:45:25 -0800

    commit tree eb30daff4073d8eb4803a558247fb5bc1afef40b

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,26 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: ["Fiction", "Non-Fiction"]
+   
+    validate :clickbait
+
+    def clickbait
+        if title.to_s.include?("Won't Believe")
+             true
+        elsif title.to_s.include?( "Secret")
+             true
+        elsif title.to_s.include?("Top #{0..100}")
+            true
+        elsif title.to_s.include?("Guess")
+             true
+        else
+            errors.add(:title, "not clickbait")
+        end
+     end
+
+     
+     
+
 end
